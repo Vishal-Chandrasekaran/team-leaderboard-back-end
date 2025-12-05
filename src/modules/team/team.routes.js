a08484c2bd64036f2be9e36e9d750ec29d99b7a2
Vishal Chandrasekaran <vishalchandrasekar@bitcot.com>
2025-12-05 11:48:22 +0530

    removed authentication on team route because it is used on unprotected routes

--- src/modules/team/team.routes.js
+++ src/modules/team/team.routes.js
@@ -1,10 +1,9 @@
 import { Router } from 'express';
 import { TeamController } from './team.controller.js';
-import { authenticate } from '../../middleware/auth.middleware.js';
 
 const router = Router();
 
-router.get('/', authenticate, TeamController.list);
+router.get('/',TeamController.list);
 
 export default router;
 
